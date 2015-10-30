b2fedf998b31bfcc9dbf129939fef80f6ca4875d
Dario Caruso <darioyzf@gmail.com>
2015-10-30 03:07:34 +0100

    oki

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ module.exports=function(){
   return new Promise(function (resolve, reject) {
     tester.test('www.yahoo.com', 80, function (err, output) {
 
-      if(err){
+      if(err||!output.success){
         tester.test('www.google.com', 80, function (err, output) {
-          if(err){
+          if(err||!output.success){
             tester.test('www.wikipedia.org', 80, function (err, output) {
-              if(err){
+              if(err||!output.success){
                 reject();
 
               } else{
