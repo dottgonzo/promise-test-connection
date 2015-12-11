0570afa0bb257385fbbcd4806b8b8aa3bb7a3266
dario <darioyzf@gmail.com>
2015-12-11 13:31:42 +0100

    oki

--- ping.sh
+++ ping.sh
@@ -2,7 +2,7 @@
 
 if [[ $OSTYPE == linux* ]]; then
 
-ping -w 1 8.8.8.8 2> /dev/null 1> /dev/null # wait 1 seconds
+ping -w 10 -c 1 8.8.8.8 2> /dev/null 1> /dev/null # wait 1 seconds
 
 if [ $? -eq 0 ]; then
 
@@ -10,23 +10,14 @@ if [ $? -eq 0 ]; then
 
 else
 
-  ping -w 5 8.8.8.8 2> /dev/null 1> /dev/null # wait 5 seconds
-  if [ $? -eq 0 ]; then
-    exit 0
-  else
-    ping -w 10 8.8.8.8 2> /dev/null 1> /dev/null # wait 10 seconds
-    if [ $? -eq 0 ]; then
-      exit 0
-    else
       exit 1
-    fi
   fi
 
 fi
 
 elif [[ $OSTYPE == darwin* ]]; then
 
-  ping -t 1 8.8.8.8 2> /dev/null 1> /dev/null # wait 1 seconds
+  ping -t 10 -c 1 8.8.8.8 2> /dev/null 1> /dev/null # wait 1 seconds
 
   if [ $? -eq 0 ]; then
 
@@ -34,20 +25,10 @@ elif [[ $OSTYPE == darwin* ]]; then
 
   else
 
-    ping -t 5 8.8.8.8 2> /dev/null 1> /dev/null # wait 5 seconds
-    if [ $? -eq 0 ]; then
-      exit 0
-    else
-      ping -t 10 8.8.8.8 2> /dev/null 1> /dev/null # wait 10 seconds
-      if [ $? -eq 0 ]; then
-        exit 0
-      else
         exit 1
-      fi
     fi
 
   fi
-
 
 
 else
