5be931cdef0defd90163cf9171c0a5883bc9c474
dario <darioyzf@gmail.com>
2015-12-11 13:33:46 +0100

    oki

--- ping.sh
+++ ping.sh
@@ -1,39 +1,11 @@
 #! /bin/bash
 
 if [[ $OSTYPE == linux* ]]; then
-
-ping -w 10 -c 1 8.8.8.8 2> /dev/null 1> /dev/null # wait 1 seconds
-
-if [ $? -eq 0 ]; then
-
-  exit 0
-
-else
-
-      exit 1
-  fi
-
-fi
-
+  ping -w 10 -c 1 8.8.8.8 2> /dev/null 1> /dev/null # wait 1 seconds
+    exit $?
 elif [[ $OSTYPE == darwin* ]]; then
-
   ping -t 10 -c 1 8.8.8.8 2> /dev/null 1> /dev/null # wait 1 seconds
-
-  if [ $? -eq 0 ]; then
-
-    exit 0
-
-  else
-
-        exit 1
-    fi
-
-  fi
-
-
+    exit $?
 else
-
   exit 1
-
-
 fi
