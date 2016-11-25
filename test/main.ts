d27be6e53bfadd31ab8a93fca23ec9eda507e74f
dario <darioyzf@gmail.com>
2016-11-25 11:10:52 +0100

    fix type

--- test/main.ts
+++ test/main.ts
@@ -1,27 +1,27 @@
-let tester = require("../index");
+import tester from "../index";
 import * as chai from "chai";
 
 
-let expect = chai.expect;
+const expect = chai.expect;
 
 
 describe("test conection", function() {
     this.timeout(20000);
     it("should return an answer", function(done) {
 
-        return tester().then(function(answer) {
+        tester().then(function(answer) {
             console.log(answer)
             expect(answer).to.be.ok;
             expect(answer).to.have.property("ping").that.is.a("boolean");
             expect(answer).to.have.property("get").that.is.a("boolean");
             expect(answer).to.have.property("server").that.is.a("boolean");
             expect(answer).to.have.property("ip").that.is.a("string");
-            return done()
+            done()
         }).catch(function(err) {
                         console.log("err")
             console.log(err)
-            expect(err).to.be.ok;
-            return done()
+            expect(err).to.not.exist;
+            done()
 
         })
 
